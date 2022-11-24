75edc774e49821a6d3dcba9d239e2e0acae3370e
adityajha0001 <rockadi2407@gmail.com>
2022-11-24 22:07:03 +0530

    component

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Banner from '../components/banner';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdn.tailwindcss.com"></script>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Coffee Delight
-        </h1>
+      <main className="py-96 pl-64">
+        <Banner/>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="">
 
       </footer>
-    </div>
+    </>
   )
 }
